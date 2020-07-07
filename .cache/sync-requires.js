d2d0cca91ecaaac1e1a69fc608c62ff851bed304
oslobird <andrey940@gmail.com>
2020-07-07 23:49:05 +0300

    gitignore commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/dedal/Documents/Early Bird/epi-group/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/dedal/Documents/Early Bird/epi-group/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/dedal/Documents/Early Bird/epi-group/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/dedal/Documents/Early Bird/epi-group/src/pages/page-2.js"))),
